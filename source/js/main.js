ba75618798c3ec8bdeba70d0d1e9fd69a1351fc3
CRYDEsigN <Cryden_da@mail.ru>
2017-09-19 10:52:49 +0300

    restyling

--- source/js/main.js
+++ source/js/main.js
@@ -6,14 +6,16 @@
  */
 
 require('./bootstrap');
-
-console.log('script init');
+//require('./../../../bower_components/gentelella/src/js/custom.js');
+//require('./../../../bower_components/gentelella/src/js/custom.js');
+//require('./../../../bower_components/gentelella/src/js/helpers/smartresize.js');
+//require('./../../../bower_components/gentelella/vendors/fastclick/lib/fastclick.js');
+//require('./../../../bower_components/gentelella/vendors/nprogress/nprogress.js');
+//require('./../../../bower_components/gentelella/vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js');
+//require('./../../../bower_components/gentelella/vendors/jquery.hotkeys/jquery.hotkeys.js');
+//require('./../../../bower_components/gentelella/vendors/google-code-prettify/src/prettify.js');
 
 require ('./modules/main/fonts_loader.js');
-require ('./modules/main/menu.js');
-//require ('./modules/main/change_theme.js');
-//require ('./modules/main/social.js');
-//require ('./modules/main/js_loader.js');
 
 //window.Vue = require('vue');
 
@@ -24,7 +26,59 @@ require ('./modules/main/menu.js');
  */
 
 //Vue.component('example', require('./components/Example.vue'));
-//
+
 //const app = new Vue({
-//    el: '#app'
+//    el: '#app',
+//    components: example
 //});
+
+
+//
+//
+// Menu component
+//
+//
+
+//Display menu function
+function menu_display() {
+    var display_menu = true;
+    console.log('menu display init');
+    $('.open_btn, .close_btn').click(function() {
+        console.log('click');
+        if (display_menu === false) {
+            $("header").animate({
+                left: "-=320",
+            }, 500);
+            $(".shadow").css("display", "none");
+            $(".open_btn").show(500);
+            $(".close_btn").css("display", "none");
+            display_menu = true;
+        } else {
+            $("header").animate({
+                left: "+=320",
+            }, 500);
+            $(".shadow").css("display", "block");
+            $(".close_btn").show(500);
+            $(".open_btn").css("display", "none");
+            display_menu = false;
+        }
+    });
+
+    $('.shadow').click(function() {
+        console.log('click');
+        if (display_menu === false) {
+            $("header").animate({
+                left: "-=320",
+            }, 500);
+            $(".shadow").css("display", "none");
+            $(".open_btn").show(500);
+            $(".close_btn").css("display", "none")
+            display_menu = true;
+    }});
+}
+
+$(document).ready(function() {
+    menu_display();
+});
+
+
